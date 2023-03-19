03317ee2c27b04eb94e8e37caee0d1330aa43601
Hyoung Jin <drj9802@gmail.com>
2023-03-20 01:24:08 +0900

    In-class exercise 2 (Calculator 구현)

--- lecture02-in-class-exercise/src/Handler.js
+++ lecture02-in-class-exercise/src/Handler.js
@@ -2,35 +2,31 @@ import React, {useState}from "react";
 
 function Handler(){
 
-    let num1=null
-    let num2=null
+    const [num1,setNum1] = useState(0);
+    const [num2,setNum2] = useState(0);
+
+    const handleNum1Change = ((e) => {
+      setNum1(parseInt(e.target.value));
+    });
+  
+    const handleNum2Change = ((e) => {
+      setNum2(parseInt(e.target.value));
+    });
 
     const[add,addHandler] = useState(num1+num2);
     const[sub,subHandler] = useState(num1-num2);
     const[mul,mulHandler] = useState(num1*num2);
     const[div,divHandler] = useState(num1/num2);
 
-    const handleNum1Change = (e) => {
-        num1 = parseInt(e.target.value);
-        console.log(num1)
-        console.log(num2)
-        console.log(typeof(num1))
-        console.log(typeof(num2))
-      };
-    
-      const handleNum2Change = (e) => {
-        num2 = parseInt(e.target.value);
-      };
-
     return(
         <>
         <input type="text" value={num1} onChange={handleNum1Change}/>
         <input type="text" value={num2} onChange={handleNum2Change}/>
         <p>
-        <button onClick={addHandler}> Add </button> {add}
-        <button onClick={subHandler}> Sub </button> {sub}
-        <button onClick={mulHandler}> Mul </button> {mul}
-        <button onClick={divHandler}> Div </button> {div}
+        <button onClick={()=>addHandler(num1+num2)}> Add </button> {add} &nbsp;&nbsp;
+        <button onClick={()=>subHandler(num1-num2)}> Sub </button> {sub} &nbsp;&nbsp;
+        <button onClick={()=>mulHandler(num1*num2)}> Mul </button> {mul} &nbsp;&nbsp;
+        <button onClick={()=>divHandler(num1/num2)}> Div </button> {div} &nbsp;&nbsp;
         </p>
         </>
     );
